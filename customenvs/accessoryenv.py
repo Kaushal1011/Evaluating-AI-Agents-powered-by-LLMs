f2f1cc39dc7f5b1ad1bb78c54c3a2d14dd1c6c1b
Kaushal <50149591+Kaushal1011@users.noreply.github.com>
2023-11-29 16:26:41 -0600

    updated accesroy env

--- customenvs/accessoryenv.py
+++ customenvs/accessoryenv.py
@@ -30,7 +30,11 @@ class WeatherEnv(gym.Env):
         # Adjust conditions based on temperatures
         # no snow if temperature is above 50
         conditions = [
-            1 if temp > 50 and condition == 0 else condition
+            1
+            if temp > 50 and condition == 0
+            else 0
+            if temp < 32 and condition == 1
+            else condition
             for temp, condition in zip(temperatures, conditions)
         ]
 
@@ -84,10 +88,43 @@ class WeatherEnv(gym.Env):
         print(f"Current State: {self.state}")
 
 
+def generate_prompt(state):
+    # Adjust conditions based on temperature
+    adjusted_conditions = [
+        "Snow" if condition == 0 else "Rain" if condition == 1 else "Sunny"
+        for temp, condition in zip(state["temperature"], state["conditions"])
+    ]
+
+    # Create the prompt
+    prompt = (
+        f"Consider the following data about weather conditions for the last 7 days:\n\n"
+    )
+    prompt += f"- Temperatures (in °F): {state['temperature']}\n"
+    prompt += f"- Weather Conditions: {adjusted_conditions}\n\n"
+    prompt += "Rules:\n"
+    prompt += "1. If the temperature is above 50°F, it cannot be snowing. In such cases, snow should be replaced with rain.\n"
+    prompt += (
+        "2. The ideal accessory choice depends on the most common weather condition:\n"
+    )
+    prompt += "   - If it's mostly sunny, pick 'Sunscreen'.\n"
+    prompt += "   - If it's mostly raining, pick 'Umbrella'.\n"
+    prompt += "   - If it's mostly snowing, pick 'Snow Boots'.\n"
+    prompt += "3. The ideal clothing choice depends on the average temperature:\n"
+    prompt += "   - Below 32°F: 'Warm' clothing.\n"
+    prompt += "   - Between 32°F and 70°F: 'Medium' clothing.\n"
+    prompt += "   - Above 70°F: 'Summer' clothing.\n\n"
+    prompt += "Based on the above data and rules, what should be the ideal accessory and clothing choice? Please explain your reasoning.\n"
+
+    return prompt
+
+
 if __name__ == "__main__":
     env = WeatherEnv()
     for _ in range(5):  # Run 5 test cases
         state = env.reset()
+        print(f"Initial State: {state}")
         action = env.action_space.sample()  # Random action
         next_state, reward, done, info = env.step(action)
         print(f"State: {state}, Action: {action}, Reward: {reward}, Done: {done}")
+        print("\n\n\n")
+        print(generate_prompt(state))
